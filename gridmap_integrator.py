0c09895fde14b4a6150938a28b56a27988d86862
goodgodgd <goodgodgd@yonsei.ac.kr>
2019-02-07 14:43:16 +0900

    refactor interpolate function

--- gridmap_integrator.py
+++ gridmap_integrator.py
@@ -4,11 +4,10 @@ import viewer
 from timeit import default_timer as timer
 import copy
 
-verbosity = True
+VERBOSITY = True
 VEHICLE_ROW_RANGE = (-2, 4)     # x(front): -1m ~ 2m
 VEHICLE_COL_RANGE = (-2, 2)     # y(left):  -1m ~ 1m
 CELL_VALUE_LIMIT = 5
-
 
 class GridMapIntegrator:
     def __init__(self):
@@ -16,8 +15,7 @@ class GridMapIntegrator:
         self.GMCOLS = int(80)
         self.GMROWS = int(120)
         self.CELLSIZE = 0.5
-        self.cur_logodd_static = np.zeros((self.GMROWS, self.GMCOLS), dtype=np.float)
-        self.cur_logodd_final = np.zeros((self.GMROWS, self.GMCOLS), dtype=np.float)
+        self.cur_logodd = np.zeros((self.GMROWS, self.GMCOLS), dtype=np.float)
         self.cur_ogm = np.ones((self.GMROWS, self.GMCOLS), dtype=np.float) * 0.5
         self.prv_logodd = np.zeros((self.GMROWS, self.GMCOLS), dtype=np.float)
         self.origin_cell = [self.GMROWS, self.GMCOLS/2]
@@ -45,7 +43,7 @@ class GridMapIntegrator:
     def integrate(self, rgb_img, grid_maps, curr_pose):
         # fill grid_maps with logodd maps before and after transformation
         grid_maps = self.transform_map(curr_pose, grid_maps)
-        # fill grid_maps with self.cur_logodd_static and cur_ogm
+        # fill grid_maps with updated logodd and occupancy map
         grid_maps = self.update_map(grid_maps)
         self.prev_pose = curr_pose
         self.dviewer.show(rgb_img, grid_maps, 1200, 0)
@@ -57,11 +55,11 @@ class GridMapIntegrator:
         prev_coords = np.matmul(tmatrix, self.cell_coords.T).T
         prev_cell_inds = self.posit_to_cell(prev_coords)
 
-        self.prv_logodd = self.interpolate(self.cur_logodd_final, prev_cell_inds, self.cell_inds)
-        grid_maps["log_bef_transform"] = copy.deepcopy(self.cur_logodd_final)
+        self.prv_logodd = self.interpolate(self.cur_logodd, prev_cell_inds, self.cell_inds)
+        grid_maps["log_bef_transform"] = copy.deepcopy(self.cur_logodd)
         grid_maps["log_aft_transform"] = copy.deepcopy(self.prv_logodd)
 
-        if verbosity:
+        if VERBOSITY:
             print("transform_map took:", timer() - start)
             print("transform matrix=\n{}".format(tmatrix))
             print(self.cell_inds.shape, self.cell_coords.shape)
@@ -83,24 +81,38 @@ class GridMapIntegrator:
         # find valid cell indices within grid map
         valid_inds = np.all((1 <= int_cells[:, 0], int_cells[:, 0] < self.GMROWS-1,
                              1 <= int_cells[:, 1], int_cells[:, 1] < self.GMCOLS-1), axis=0)
-        if verbosity:
+        if VERBOSITY:
             print("valid inds\n{}\n{}".format(int_cells[3000:3200:20].T, valid_inds[3000:3200:20].T))
         # extract valid src cells and integer cells
         valid_src_cells = src_cells[valid_inds]
         valid_int_cells = int_cells[valid_inds]
 
+        # calculate 9 neighbor cell coordinates and their weights for each valid cell
+        neighbor_values, weights = \
+            self.find_neighbors_n_weights(gridmap, valid_src_cells, valid_int_cells)
+        # calculate weighted sum of neighbor values as dst cell values
+        dst_values = self.weight_sum(neighbor_values, weights)
+
+        interp_map = np.zeros(gridmap.shape)
+        # extract dst cells and set dst_values onto dst cells
+        valid_dst_cells = dst_cells[valid_inds]
+        dst_inds = tuple([tuple(cell) for cell in valid_dst_cells.T])
+        interp_map[dst_inds] = dst_values
+        return interp_map
+
+    @staticmethod
+    def find_neighbors_n_weights(gridmap, src_cells, int_cells):
         # relative positions of neighbor cells
         relposits = np.array([[-1, -1], [-1, 0], [-1, 1],
                               [0, -1], [0, 0], [0, 1],
                               [1, -1], [1, 0], [1, 1]])
-        # calculate 9 neighbor cell coordinates and their weights for each valid cell
         neighbor_values = []
         weights = []
         for relpos in relposits:
             # calculate neighbor cells of all valid cells by relative position 'relpos'
-            neighbor_cells = valid_int_cells + np.tile(relpos, (len(valid_int_cells), 1))
+            neighbor_cells = int_cells + np.tile(relpos, (len(int_cells), 1))
             # calculate neighbor weights that is close to 1 when it is close to src cell
-            nei_weight = 1 - np.abs(neighbor_cells - valid_src_cells)
+            nei_weight = 1 - np.abs(neighbor_cells - src_cells)
             # ignore weight < 0
             nei_weight[nei_weight < 0] = 0
             # multiply row weight and column weight
@@ -114,27 +126,23 @@ class GridMapIntegrator:
             # print("cell values\n", cell_values.shape, cell_values[:10])
             neighbor_values.append(cell_values)
 
+        return neighbor_values, weights
+
+    @staticmethod
+    def weight_sum(values, weights):
         # nomalize weights
         weights = np.stack(weights, axis=1)
         weight_sum = np.sum(weights, axis=1)
-        weights = weights / np.tile(weight_sum, (len(relposits), 1)).T
+        weights = weights / np.tile(weight_sum, (weights.shape[1], 1)).T
         # interpolated value is weight sum of neighbor cell values
-        neighbor_values = np.stack(neighbor_values, axis=1)
-        dst_values = np.sum(neighbor_values * weights, axis=1)
-        if verbosity:
-            print("neighbor_values\n{}\nweights\n{}\ndst_values\n{}".format(
-                neighbor_values[3000:3200:20], weights[3000:3200:20], dst_values[3000:3200:20]))
+        values = np.stack(values, axis=1)
+        dst_values = np.sum(values * weights, axis=1)
 
-        interp_map = np.zeros(gridmap.shape)
-        # extract dst cells and set dst_values onto dst cells
-        valid_dst_cells = dst_cells[valid_inds]
-        dst_inds = tuple([tuple(cell) for cell in valid_dst_cells.T])
-        interp_map[dst_inds] = dst_values
-        return interp_map
+        if VERBOSITY:
+            print("values\n{}\nweights\n{}\ndst_values\n{}".format(
+                values[3000:3200:20], weights[3000:3200:20], dst_values[3000:3200:20]))
 
-    def outside_gridmap(self, cell):
-        return (cell[0] < 0 or cell[0] >= self.GMROWS
-                or cell[1] < 0 or cell[1] >= self.GMCOLS)
+        return dst_values
 
     def update_map(self, grid_maps, weights=None):
         if not weights:
@@ -143,21 +151,19 @@ class GridMapIntegrator:
         grid_maps["cam_yol_occ"] = self.detection_to_occupancy_log(grid_maps["cam_yol"])
 
         static_keys = ["cam_fcn", "lid_seg"]
-        self.cur_logodd_static = self.prv_logodd
+        self.cur_logodd = self.prv_logodd
         for key in static_keys:
-            self.cur_logodd_static += grid_maps[key] * weights[key]
-        self.cur_logodd_static = np.clip(self.cur_logodd_static,
-                                         -CELL_VALUE_LIMIT, CELL_VALUE_LIMIT)
+            self.cur_logodd += grid_maps[key] * weights[key]
+        self.cur_logodd = np.clip(self.cur_logodd, -CELL_VALUE_LIMIT, CELL_VALUE_LIMIT)
 
         dynamic_keys = ["cam_yol_occ"]
-        self.cur_logodd_final = copy.deepcopy(self.cur_logodd_static)
         for key in dynamic_keys:
-            self.cur_logodd_final = np.where(grid_maps[key] > 0,
-                                             -grid_maps[key] * weights[key],
-                                             self.cur_logodd_final)
+            self.cur_logodd = np.where(grid_maps[key] > 0,
+                                       -grid_maps[key] * weights[key],
+                                       self.cur_logodd)
 
-        self.cur_ogm = self.logodd_to_prob(self.cur_logodd_final)
-        grid_maps["updated_log_odd"] = copy.deepcopy(self.cur_logodd_final)
+        self.cur_ogm = self.logodd_to_prob(self.cur_logodd)
+        grid_maps["updated_log_odd"] = copy.deepcopy(self.cur_logodd)
         grid_maps["occupancy_map"] = copy.deepcopy(self.cur_ogm)
         return grid_maps
 
@@ -188,7 +194,7 @@ def main():
         images = kitti_loader.get_rgb(ind)
         gmaps = kitti_loader.get_gmaps(ind)
         transform = kitti_loader.get_pose(ind)
-        if verbosity:
+        if VERBOSITY:
             print("\n\nindex={}".format(ind))
 
         grid_mapper.integrate(images["rgbL"], gmaps, transform)
